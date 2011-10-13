ba3265e17cc7e92ed2085d5a9df4525882f9b9a4
Tobias Munk <schmunk@usrbin.de>
2011-10-13 19:34:31 +0200

    updated popup view

--- views/ckeditor/index.php
+++ views/ckeditor/index.php
@@ -23,27 +23,37 @@ data: $(this).serialize()
 
 <h1> <?php echo Yii::t('app', 'Ckeditor'); ?> P3 Medias</h1>
 
-<?php 
- echo CHtml::link(Yii::t('app', 'Advanced Search'), '#', array('class' => 'search-button')); 
-echo " ";
-echo CHtml::link('Upload',array('/p3media/import/upload'),array('target'=>'_blank'));
-?>
+<div class="span-8">
+	<h2>Menu</h2>
+	<p>
+		<?php
+		echo CHtml::link(Yii::t('app', 'Advanced Search'), '#', array('class' => 'search-button'));
+		echo " ";
+		echo CHtml::link('Upload', array('/p3media/import/upload'), array('target' => '_blank'));
+		echo " ";
+		echo CHtml::link('Reload', null, array('target' => '_blank', 'onclick' => 'location.reload();'));
+		?>
+	</p>
 
-<div class="search-form" style="display:none">
-	<?php
-	$this->renderPartial('/p3Media/_search', array(
-		'model' => $model,
-	));
-	?>
+	<div class="search-form" style="display:none">
+		<?php
+		$this->renderPartial('/p3Media/_search', array(
+			'model' => $model,
+		));
+		?>
+	</div>
 </div>
 
-<div>
-	<?php
-		foreach($this->module->params['presets'] AS $key => $preset) {
-			$data[$key] = (isset($preset['name']))?$preset['name']:$key;
+<div class="span-8 last">
+	<h2>Preset</h2>
+	<p>
+		<?php
+		foreach ($this->module->params['presets'] AS $key => $preset) {
+			$data[$key] = (isset($preset['name'])) ? $preset['name'] : $key;
 		}
 		echo Chtml::dropDownList("preset", null, $data);
-	?>
+		?>
+	</p>
 </div>
 
 <?php
@@ -64,11 +74,11 @@ $this->widget('zii.widgets.grid.CGridView', array(
 				',
 		),
 		'id',
-		/*array(
-			'name' => 'metaData.treeParent_id',
-			'value' => 'CHtml::value($data,\'metaData.treeParent_id\')',
-			'filter' => CHtml::listData(P3Media::model()->with('metaData')->findAll(), 'metaData.id', 'recordTitle'),
-		),*/
+		/* array(
+		  'name' => 'metaData.treeParent_id',
+		  'value' => 'CHtml::value($data,\'metaData.treeParent_id\')',
+		  'filter' => CHtml::listData(P3Media::model()->with('metaData')->findAll(), 'metaData.id', 'recordTitle'),
+		  ), */
 		'title',
 		'description',
 		'mimeType',
@@ -86,14 +96,13 @@ $this->widget('zii.widgets.grid.CGridView', array(
 
 <script type="text/javascript">
 	function select(id){
-		if (confirm('Select #'+id+'?')) {		
+		if($('#preset').val() == '') {
+			alert('Please choose an image preset.');
+			return false;
+		}
+		var preset = $('#preset').val();
+		if (confirm('Select #'+id+' as \''+preset+'\'?')) {		
 			//alert(id+'-'+preset);
-			if($('#preset').val() == '') {
-			    alert('Please choose an image preset.');
-			    return false;
-			} else {
-				var preset = $('#preset').val();
-			}
 			url = '<?php echo CController::createUrl('/p3media/file/image', array('id' => '__ID__', 'preset' => '__PRESET__')) ?>';
 			url = url.replace('__PRESET__', preset);
 			url = url.replace('__ID__', id);
