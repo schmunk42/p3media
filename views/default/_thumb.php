ea2e835690702fa7d3018af546f642093ebf2411
Tobias Munk <schmunk@usrbin.de>
2012-10-08 19:16:07 +0200

    cleanup

--- views/default/_thumb.php
+++ views/default/_thumb.php
@@ -1,23 +1,28 @@
 <li class="span3">
     <span class="thumbnail" rel="tooltip" data-title="<?php echo "#" . $data->id ?>">
     <h5><?php echo $data->title ?></h5>
-<div>
-    <?php $this->widget('bootstrap.widgets.TbButtonGroup',
-                        array('buttons' => array(
-                            array(
-                                'icon' => 'eye-open',
-                                'url' => array('p3Media/view', 'id' => $data->id, 'returnUrl' => $this->createUrl("", $_GET))
-                            ),
-                            array(
-                                'icon' => 'pencil',
-                                'url' => array('p3Media/update', 'id' => $data->id, 'returnUrl' => $this->createUrl("", $_GET))
-                            ),
-                            array(
-                                'icon' => 'info-sign',
-                                'url' => array('p3MediaMeta/update', 'id' => $data->id, 'returnUrl' => $this->createUrl("", $_GET))
-                            ))
-                        )); ?>
-</div>
+
+    <div>
+        <?php $this->widget('bootstrap.widgets.TbButtonGroup',
+                            array('buttons' => array(
+                                array(
+                                    'icon' => 'eye-open',
+                                    'url' => array('p3Media/view', 'id' => $data->id,
+                                                   'returnUrl' => $this->createUrl("", $_GET))
+                                ),
+                                array(
+                                    'icon' => 'pencil',
+                                    'url' => array('p3Media/update', 'id' => $data->id,
+                                                   'returnUrl' => $this->createUrl("", $_GET))
+                                ),
+                                array(
+                                    'icon' => 'info-sign',
+                                    'url' => array('p3MediaMeta/update', 'id' => $data->id,
+                                                   'returnUrl' => $this->createUrl("", $_GET))
+                                ))
+                            )); ?>
+    </div>
+
         <?php
         echo CHtml::link(
             CHtml::image(
@@ -31,6 +36,5 @@
         ?>
 
         <div class="ui-helper-clearfix"></div>
-    </span>
 
 </li>
