8cc7ffa35d1644ebe53552cac007a129c56ede72
Tobias Munk <schmunk@usrbin.de>
2012-10-11 19:49:56 +0200

    updated foreign key constraints

---
+++ migrations/m121011_170518_fk_delete_cascade.php
@@ -0,0 +1,33 @@
+<?php
+
+class m121011_170518_fk_delete_cascade extends EDbMigration
+{
+    public function up()
+    {
+        if ((Yii::app()->db->schema instanceof CSqliteSchema) == false):
+
+            $this->dropForeignKey('fk_p3_media_id', 'p3_media_meta');
+            $this->addForeignKey('fk_p3_media_id', 'p3_media_meta', 'id', 'p3_media', 'id', 'CASCADE', 'CASCADE');
+            $this->dropForeignKey('fk_p3_media_meta_treeParent_id', 'p3_media_meta');
+            $this->addForeignKey('fk_p3_media_meta_treeParent_id', 'p3_media_meta', 'treeParent_id', 'p3_media_meta', 'id', 'RESTRICT', 'RESTRICT');
+
+        endif;
+    }
+
+    public function down()
+    {
+        echo "m121011_160518_fk_delete_cascade does not support migration down.\n";
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to do migration with transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
