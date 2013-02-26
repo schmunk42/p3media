4e53edac11364e66da7ee83903e7f8395db2b7d8
Tobias Munk <schmunk@usrbin.de>
2013-02-26 02:21:19 +0100

    added media selection widget, based on select2

---
+++ components/P3MediaSelect.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tobias
+ * Date: 25.02.13
+ * Time: 17:11
+ * To change this template use File | Settings | File Templates.
+ */
+class P3MediaSelect extends CWidget
+{
+    public $model;
+    public $attribute;
+
+    function run()
+    {
+        echo CHtml::activeTextField($this->model,
+                                    $this->attribute,
+                                    array(
+                                         'data-init-text' => ($this->getMediaModel())?$this->getMediaModel()->title:null
+                                    )
+        );
+
+        $id = get_class($this->model) . "_" . $this->attribute;
+        $this->widget('ESelect2',
+                      array(
+                           'selector' => "#" . $id,
+                           'options'  => array(
+                               'width'              => '100%',
+                               'height'             => '500px',
+                               'placeholder'        => 'Search Media File',
+                               'minimumInputLength' => 0,
+                               'ajax'               => array(
+                                   'url'      => Yii::app()->controller->createUrl('/p3media/p3Media/ajaxSearch'),
+                                   'dataType' => 'jsonp',
+                                   'data'     => 'js: function(term,page) {
+                                                        return {
+                                                            q: term,
+                                                            page_limit: 10,
+                                                        };
+                                                  }',
+                                   'results'  => 'js: function(data,page){
+                                                      return {results: data};
+                                                  }',
+                               ),
+                               'formatResult'       => 'js:function(data){
+                                    var markup = "<table class=\"data-result\"><tr>";
+                                    if (data.image !== undefined) {
+                                        markup += "<td class=\"data-image\">" + data.image + "</td>";
+                                    }
+                                    markup += "<td class=\"data-info\"><div class=\"data-title\">" + data.title + "</div>";
+                                    markup += "</td></tr></table>";
+                                    return markup;
+                                }',
+                               'formatSelection'    => 'js: function(data) {
+                                    return data.title;
+                                }',
+                               'initSelection'      => 'js: function(element, callback) {
+                                    var elementText = $(element).data("init-text");
+                                    callback({"title":elementText});
+                               }'
+                           ),
+                      ));
+    }
+
+    private function getMediaModel()
+    {
+        $model = P3Media::model()->findByPk($this->model->{$this->attribute});
+        return $model;
+    }
+}
--- controllers/P3MediaController.php
+++ controllers/P3MediaController.php
@@ -6,95 +6,102 @@ class P3MediaController extends Controller
     public $defaultAction = "admin";
     public $scenario = "crud";
 
-public function filters() {
-	return array(
-			'accessControl',
-			);
-}
+    public function filters()
+    {
+        return array(
+            'accessControl',
+        );
+    }
 
-public function accessRules() {
-	return array(
-			array('allow',
-				'actions'=>array('create','ajaxUpdate','update','delete','admin','view'),
-				'roles'=>array('P3media.P3Media.*'),
-				),
-			array('deny',
-				'users'=>array('*'),
-				),
-			);
-}
+    public function accessRules()
+    {
+        return array(
+            array('allow',
+                  'actions' => array('create', 'update', 'delete', 'admin', 'view', 'ajaxUpdate', 'ajaxSearch'),
+                  'roles'   => array('P3media.P3Media.*'),
+            ),
+            array('deny',
+                  'users' => array('*'),
+            ),
+        );
+    }
 
-    public function beforeAction($action){
+    public function beforeAction($action)
+    {
         parent::beforeAction($action);
         // map identifcationColumn to id
         if (!isset($_GET['id']) && isset($_GET['path'])) {
-            $model=P3Media::model()->find('path = :path', array(
-            ':path' => $_GET['path']));
+            $model = P3Media::model()->find('path = :path', array(
+                                                                 ':path' => $_GET['path']));
             if ($model !== null) {
                 $_GET['id'] = $model->id;
-            } else {
+            }
+            else {
                 throw new CHttpException(400);
             }
         }
         if ($this->module !== null) {
-            $this->breadcrumbs[$this->module->Id] = array('/'.$this->module->Id);
+            $this->breadcrumbs[$this->module->Id] = array('/' . $this->module->Id);
         }
+
         return true;
     }
 
     public function actionView($id)
     {
         $model = $this->loadModel($id);
-        $this->render('view',array('model' => $model,));
+        $this->render('view', array('model' => $model,));
     }
 
     public function actionCreate()
     {
-        $model = new P3Media;
+        $model           = new P3Media;
         $model->scenario = $this->scenario;
 
-                $this->performAjaxValidation($model, 'p3-media-form');
-    
-        if(isset($_POST['P3Media'])) {
+        $this->performAjaxValidation($model, 'p3-media-form');
+
+        if (isset($_POST['P3Media'])) {
             $model->attributes = $_POST['P3Media'];
 
             try {
-                if($model->save()) {
+                if ($model->save()) {
                     if (isset($_GET['returnUrl'])) {
                         $this->redirect($_GET['returnUrl']);
-                    } else {
-                        $this->redirect(array('view','id'=>$model->id));
+                    }
+                    else {
+                        $this->redirect(array('view', 'id' => $model->id));
                     }
                 }
             } catch (Exception $e) {
                 $model->addError('path', $e->getMessage());
             }
-        } elseif(isset($_GET['P3Media'])) {
+        }
+        elseif (isset($_GET['P3Media'])) {
             $model->attributes = $_GET['P3Media'];
         }
 
-        $this->render('create',array( 'model'=>$model));
+        $this->render('create', array('model' => $model));
     }
 
 
     public function actionUpdate($id)
     {
-        $model = $this->loadModel($id);
+        $model           = $this->loadModel($id);
         $model->scenario = $this->scenario;
 
-                $this->performAjaxValidation($model, 'p3-media-form');
-        
-        if(isset($_POST['P3Media']))
-        {
+        $this->performAjaxValidation($model, 'p3-media-form');
+
+        if (isset($_POST['P3Media'])) {
             $model->attributes = $_POST['P3Media'];
 
 
             try {
-                if($model->save()) {
+                if ($model->save()) {
                     if (isset($_GET['returnUrl'])) {
                         $this->redirect($_GET['returnUrl']);
-                    } else {
-                        $this->redirect(array('view','id'=>$model->id));
+                    }
+                    else {
+                        $this->redirect(array('view', 'id' => $model->id));
                     }
                 }
             } catch (Exception $e) {
@@ -102,69 +109,87 @@ public function accessRules() {
             }
         }
 
-        $this->render('update',array('model'=>$model,));
+        $this->render('update', array('model' => $model,));
     }
 
     public function actionAjaxUpdate()
     {
         Yii::import('EditableSaver'); //or you can add import 'ext.editable.*' to config
-        $es = new EditableSaver('P3Media');  // classname of model to be updated
+        $es = new EditableSaver('P3Media'); // classname of model to be updated
         $es->update();
     }
 
     public function actionDelete($id)
     {
-        if(Yii::app()->request->isPostRequest)
-        {
+        if (Yii::app()->request->isPostRequest) {
             try {
                 $this->loadModel($id)->delete();
             } catch (Exception $e) {
-                throw new CHttpException(500,$e->getMessage());
+                throw new CHttpException(500, $e->getMessage());
             }
 
-            if(!isset($_GET['ajax']))
-            {
+            if (!isset($_GET['ajax'])) {
                 if (isset($_GET['returnUrl'])) {
                     $this->redirect($_GET['returnUrl']);
-                } else {
+                }
+                else {
                     $this->redirect(array('admin'));
                 }
             }
         }
-        else
-            throw new CHttpException(400,Yii::t('app', 'Invalid request. Please do not repeat this request again.'));
+        else {
+            throw new CHttpException(400, Yii::t('app', 'Invalid request. Please do not repeat this request again.'));
+        }
     }
 
     public function actionIndex()
     {
-        $dataProvider=new CActiveDataProvider('P3Media');
-        $this->render('index',array('dataProvider'=>$dataProvider,));
+        $dataProvider = new CActiveDataProvider('P3Media');
+        $this->render('index', array('dataProvider' => $dataProvider,));
     }
 
     public function actionAdmin()
     {
-        $model=new P3Media('search');
+        $model = new P3Media('search');
         $model->unsetAttributes();
 
-        if(isset($_GET['P3Media'])) {
+        if (isset($_GET['P3Media'])) {
             $model->attributes = $_GET['P3Media'];
         }
 
-        $this->render('admin',array('model'=>$model,));
+        $this->render('admin', array('model' => $model,));
+    }
+
+    public function actionAjaxSearch()
+    {
+        $model        = new P3Media('search');
+        $model->title = $_GET['q'];
+        $result       = array();
+        foreach ($model->search()->getData() AS $data) {
+            $result[] = array(
+                'id'    => $data->id,
+                'title' => $data->title,
+                'image' => $data->image('p3media-upload')
+            );
+        }
+        echo $_GET['callback'] . "(";
+        echo CJSON::encode($result);
+        echo ")";
     }
 
     public function loadModel($id)
     {
-        $model=P3Media::model()->findByPk($id);
-        if($model===null)
-            throw new CHttpException(404,Yii::t('app', 'The requested page does not exist.'));
+        $model = P3Media::model()->findByPk($id);
+        if ($model === null) {
+            throw new CHttpException(404, Yii::t('app', 'The requested page does not exist.'));
+        }
+
         return $model;
     }
 
     protected function performAjaxValidation($model)
     {
-        if(isset($_POST['ajax']) && $_POST['ajax']==='p3-media-form')
-        {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'p3-media-form') {
             echo CActiveForm::validate($model);
             Yii::app()->end();
         }
