8f40cd9a51d5e4e8fb23091e233aef2f27a2889c
Thomas Schlich <t.schlich@herzogkommunikation.de>
2013-07-17 14:28:22 +0200

    removed size change on field "title"

--- migrations/m130717_101010_add_nameId_column.php
+++ migrations/m130717_101010_add_nameId_column.php
@@ -6,12 +6,10 @@ class m130717_101010_add_nameId_column extends EDbMigration
     {
         $this->addColumn('p3_media', 'nameId', 'VARCHAR(64)');
         $this->createIndex('p3_media_nameId_unique', 'p3_media', 'nameId', true);
-        $this->alterColumn('p3_media', 'title', 'varchar(255)');
     }
 
     public function down()
     {
         $this->dropColumn('p3_media', 'nameId');
-        $this->alterColumn('p3_media', 'title', 'varchar(32)');
     }
 }
