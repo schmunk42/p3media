871527fb2ffe2909094f0b766f6ed992f906472c
Tobias Munk <schmunk@usrbin.de>
2013-10-07 17:52:30 +0200

    added behaviors

--- models/P3MediaTranslation.php
+++ models/P3MediaTranslation.php
@@ -20,14 +20,36 @@ class P3MediaTranslation extends BaseP3MediaTranslation
 
     public function getItemLabel()
     {
-        return parent::getItemLabel();
+        return $this->language;
     }
 
     public function behaviors()
     {
         return array_merge(
             parent::behaviors(),
-            array()
+            array(
+                 'Access'           => array(
+                     'class' => '\PhAccessBehavior'
+                 ),
+                 'LoggableBehavior' => array(
+                     'class'   => 'vendor.sammaye.auditrail2.behaviors.LoggableBehavior',
+                     'ignored' => array(
+                         'created_at',
+                         'updated_at',
+                     )
+                 ),
+                 'Status'           => array(
+                     'class'       => 'vendor.yiiext.status-behavior.EStatusBehavior',
+                     'statusField' => 'status'
+                 ),
+                 'Timestamp'        => array(
+                     'class'               => 'zii.behaviors.CTimestampBehavior',
+                     'createAttribute'     => 'created_at',
+                     'updateAttribute'     => 'updated_at',
+                     'setUpdateOnCreate'   => true,
+                     'timestampExpression' => "date_format(date_create(),'Y-m-d H:i:s');",
+                 ),
+            )
         );
     }
 
@@ -42,4 +64,66 @@ class P3MediaTranslation extends BaseP3MediaTranslation
         );
     }
 
+    public function getStatusCssClass()
+    {
+        $key = ($this->isNewRecord) ? 'new' : $this->getAttribute('status');
+        return $this->_statusCssClassMap[$key];
+    }
+
+    /**
+     * @return array list of options
+     */
+    public static function optsStatus()
+    {
+        $model = P3Page::model();
+        return array_combine($model->Status->statuses, $model->Status->statuses);
+    }
+
+    /**
+     * @return array list of options
+
+    public static function optsAccessOwner()
+    {
+    return self::model()->Access->getAccessOwner();
+    }*/
+
+    /**
+     * @return array list of options
+     */
+    public static function optsLanguage()
+    {
+        return (array) Yii::app()->params['languages'];
+    }
+
+    /**
+     * @return array list of options
+     */
+    public static function optsAccessRead()
+    {
+        return self::model()->Access->getAccessRoles();
+    }
+
+    /**
+     * @return array list of options
+     */
+    public static function optsAccessUpdate()
+    {
+        return self::model()->Access->getAccessRoles();
+    }
+
+    /**
+     * @return array list of options
+     */
+    public static function optsAccessDelete()
+    {
+        return self::model()->Access->getAccessRoles();
+    }
+
+    /**
+     * @return array list of options
+     */
+    public static function optsAccessAppend()
+    {
+        return self::model()->Access->getAccessRoles();
+    }
 }
