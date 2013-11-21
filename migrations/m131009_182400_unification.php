7c35235753158b145c319e02ab6cd8f2dcd04c97
Tobias Munk <schmunk@usrbin.de>
2013-11-21 02:01:20 +0100

    updated deps, fixed https://github.com/phundament/app/issues/98

--- migrations/m131009_182400_unification.php
+++ migrations/m131009_182400_unification.php
@@ -26,77 +26,77 @@ class m131009_182400_unification extends EDbMigration
         );
 
         $this->createTable(
-            "_p3_media_v0_16",
-            array(
-                 "id"                     => "pk",
-                 // yiiext/status-behavior
-                 "status"                 => "varchar(32) NOT NULL",
-                 "type"                   => "VARCHAR(64) NOT NULL DEFAULT 'file'",
-                 "name_id"                => "VARCHAR(64)",
-                 // mikehaertl/translatable (defaults)
-                 "default_title"          => "varchar(255) NOT NULL",
-                 "default_description"    => "text",
-                 // schmunk42/adjacency-list-behavior
-                 "tree_parent_id"         => "int(11)",
-                 "tree_position"          => "int(11)",
-                 "custom_data_json"       => "text",
-                 "original_name"          => "varchar(128)",
-                 "path"                   => "varchar(255)",
-                 "hash"                   => "varchar(64)",
-                 "mime_type"              => "varchar(128)",
-                 "size"                   => "int(11)",
-                 "info_php_json"          => "text",
-                 "info_image_magick_json" => "text",
-                 // schmunk42/yii-access
-                 "access_owner"           => "varchar(64) NOT NULL",
-                 "access_domain"          => "varchar(8) NOT NULL",
-                 "access_read"            => "varchar(256)",
-                 "access_update"          => "varchar(256)",
-                 "access_delete"          => "varchar(256)",
-                 "access_append"          => "varchar(256)",
-                 // copy behavior
-                 "copied_from_id"         => "int(11)",
-                 // time
-                 "created_at"             => "datetime NOT NULL DEFAULT '0000-00-00 00:00:00'",
-                 "updated_at"             => "datetime NOT NULL DEFAULT '0000-00-00 00:00:00'",
-                 "FOREIGN KEY(tree_parent_id) REFERENCES _p3_media_v0_16(id) ON DELETE RESTRICT ON UPDATE CASCADE",
-            ),
-            $options
+             "_p3_media_v0_16",
+                 array(
+                      "id"                     => "pk",
+                      // yiiext/status-behavior
+                      "status"                 => "varchar(32) NOT NULL",
+                      "type"                   => "VARCHAR(64) NOT NULL DEFAULT 'file'",
+                      "name_id"                => "VARCHAR(64)",
+                      // mikehaertl/translatable (defaults)
+                      "default_title"          => "varchar(255) NOT NULL",
+                      "default_description"    => "text",
+                      // schmunk42/adjacency-list-behavior
+                      "tree_parent_id"         => "int(11)",
+                      "tree_position"          => "int(11)",
+                      "custom_data_json"       => "text",
+                      "original_name"          => "varchar(128)",
+                      "path"                   => "varchar(255)",
+                      "hash"                   => "varchar(64)",
+                      "mime_type"              => "varchar(128)",
+                      "size"                   => "int(11)",
+                      "info_php_json"          => "text",
+                      "info_image_magick_json" => "text",
+                      // schmunk42/yii-access
+                      "access_owner"           => "varchar(64) NOT NULL",
+                      "access_domain"          => "varchar(8) NOT NULL",
+                      "access_read"            => "varchar(256)",
+                      "access_update"          => "varchar(256)",
+                      "access_delete"          => "varchar(256)",
+                      "access_append"          => "varchar(256)",
+                      // copy behavior
+                      "copied_from_id"         => "int(11)",
+                      // time
+                      "created_at"             => "datetime NULL DEFAULT NULL",
+                      "updated_at"             => "datetime NULL DEFAULT NULL",
+                      "FOREIGN KEY(tree_parent_id) REFERENCES _p3_media_v0_16(id) ON DELETE RESTRICT ON UPDATE CASCADE",
+                 ),
+                 $options
         );
 
         // Schema for table 'p3_media_translation'
         $this->createTable(
-            "_p3_media_translation_v0_16",
-            array(
-                 "id"             => "pk",
-                 "p3_media_id"    => "int(11) NOT NULL",
-                 "status"         => "varchar(32) NOT NULL",
-                 "language"       => "varchar(8) NOT NULL",
-                 "title"          => "varchar(255) NOT NULL",
-                 "description"    => "text",
-                 // schmunk42/yii-access
-                 "access_owner"   => "varchar(64) NOT NULL",
-                 #"access_domain"       => "varchar(8)", // * DOMAIN ????
-                 "access_read"    => "varchar(256)",
-                 "access_update"  => "varchar(256)",
-                 "access_delete"  => "varchar(256)",
-                 #"access_append"       => "varchar(256)",
-                 // copy behavior
-                 "copied_from_id" => "int(11)",
-                 // time
-                 "created_at"     => "datetime NOT NULL DEFAULT '0000-00-00 00:00:00'",
-                 "updated_at"     => "datetime NOT NULL DEFAULT '0000-00-00 00:00:00'",
-                 "FOREIGN KEY(p3_media_id) REFERENCES _p3_media_v0_16(id) ON DELETE CASCADE ON UPDATE CASCADE"
-            ),
-            $options
+             "_p3_media_translation_v0_16",
+                 array(
+                      "id"             => "pk",
+                      "p3_media_id"    => "int(11) NOT NULL",
+                      "status"         => "varchar(32) NOT NULL",
+                      "language"       => "varchar(8) NOT NULL",
+                      "title"          => "varchar(255) NOT NULL",
+                      "description"    => "text",
+                      // schmunk42/yii-access
+                      "access_owner"   => "varchar(64) NOT NULL",
+                      #"access_domain"       => "varchar(8)", // * DOMAIN ????
+                      "access_read"    => "varchar(256)",
+                      "access_update"  => "varchar(256)",
+                      "access_delete"  => "varchar(256)",
+                      #"access_append"       => "varchar(256)",
+                      // copy behavior
+                      "copied_from_id" => "int(11)",
+                      // time
+                      "created_at"     => "datetime NULL DEFAULT NULL",
+                      "updated_at"     => "datetime NULL DEFAULT NULL",
+                      "FOREIGN KEY(p3_media_id) REFERENCES _p3_media_v0_16(id) ON DELETE CASCADE ON UPDATE CASCADE"
+                 ),
+                 $options
         );
 
         $this->createIndex('p3_media_name_id_unique', '_p3_media_v0_16', 'name_id', true);
         $this->createIndex(
-            'p3_media_translation_id_language_unique',
-            '_p3_media_translation_v0_16',
-            'p3_media_id, language',
-            true
+             'p3_media_translation_id_language_unique',
+                 '_p3_media_translation_v0_16',
+                 'p3_media_id, language',
+                 true
         );
 
         // JOIN all three existing tables, use the first translation as default values
@@ -114,7 +114,7 @@ class m131009_182400_unification extends EDbMigration
             $status[$row['id']] = ($row['status']) ? $statusMap[$row['status']] : 'draft';
 
             echo $row['type'];
-            $type[$row['id']] = ($row['type'] == 1)?'file':'directory';
+            $type[$row['id']] = ($row['type'] == 1) ? 'file' : 'directory';
 
             $info = CJSON::decode($row['info']);
             if (isset($info[0])) {
@@ -127,41 +127,43 @@ class m131009_182400_unification extends EDbMigration
 
 
             $this->insert(
-                "_p3_media_v0_16",
-                array(
-                     "id"                     => $row['id'],
-                     "name_id"                => $row['nameId'],
-                     // mikehaertl/translatable (defaults)
-                     "type"                   => $type[$row['id']],
-                     // mikehaertl/translatable (defaults)
-                     "default_title"          => $row['title'],
-                     "default_description"    => $row['description'],
-                     // schmunk42/adjacency-list-behavior
-                     "tree_parent_id"         => $row['treeParent_id'],
-                     "tree_position"          => $row['treePosition'],
-                     "custom_data_json"       => $row['customData'],
-                     "original_name"          => $row['originalName'],
-                     "path"                   => $row['path'],
-                     "hash"                   => $row['md5'],
-                     "mime_type"              => $row['mimeType'],
-                     "size"                   => $row['size'],
-                     "info_php_json"          => $info_php_json,
-                     "info_image_magick_json" => $info_image_magick_json,
-                     // yiiext/status-behavior
-                     "status"                 => $status[$row['id']],
-                     // schmunk42/yii-access
-                     "access_owner"           => $owner[$row['id']],
-                     "access_domain"          => $row['language'],
-                     "access_read"            => $row['checkAccessRead'],
-                     "access_update"          => $row['checkAccessUpdate'],
-                     "access_delete"          => $row['checkAccessDelete'],
-                     #"access_append"       => $row['checkAccessUpdate'],
-                     // copy behavior
-                     "copied_from_id"         => null,
-                     // time
-                     "created_at"             => $row['createdAt'],
-                     "updated_at"             => $row['modifiedAt'],
-                )
+                 "_p3_media_v0_16",
+                     array(
+                          "id"                     => $row['id'],
+                          "name_id"                => $row['nameId'],
+                          // mikehaertl/translatable (defaults)
+                          "type"                   => $type[$row['id']],
+                          // mikehaertl/translatable (defaults)
+                          "default_title"          => $row['title'],
+                          "default_description"    => $row['description'],
+                          // schmunk42/adjacency-list-behavior
+                          "tree_parent_id"         => $row['treeParent_id'],
+                          "tree_position"          => $row['treePosition'],
+                          "custom_data_json"       => $row['customData'],
+                          "original_name"          => $row['originalName'],
+                          "path"                   => $row['path'],
+                          "hash"                   => $row['md5'],
+                          "mime_type"              => $row['mimeType'],
+                          "size"                   => $row['size'],
+                          "info_php_json"          => $info_php_json,
+                          "info_image_magick_json" => $info_image_magick_json,
+                          // yiiext/status-behavior
+                          "status"                 => $status[$row['id']],
+                          // schmunk42/yii-access
+                          "access_owner"           => $owner[$row['id']],
+                          "access_domain"          => $row['language'],
+                          "access_read"            => $row['checkAccessRead'],
+                          "access_update"          => $row['checkAccessUpdate'],
+                          "access_delete"          => $row['checkAccessDelete'],
+                          #"access_append"       => $row['checkAccessUpdate'],
+                          // copy behavior
+                          "copied_from_id"         => null,
+                          // time
+                          "created_at"             => ($row['createdAt'] && $row['createdAt'] != '0000-00-00 00:00:00') ?
+                                  $row['createdAt'] : null,
+                          "updated_at"             => ($row['modifiedAt'] && $row['modifiedAt'] != '0000-00-00 00:00:00') ?
+                                  $row['modifiedAt'] : null,
+                     )
             );
 
         }
